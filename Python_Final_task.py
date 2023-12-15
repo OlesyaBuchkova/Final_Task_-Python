59de3a37a0cecc7148f00464ae8179442cb5c995
olesya <nearolesya@gmil.com>
2023-12-15 17:01:50 +0300

    add dz

--- Python_Final_task.py
+++ Python_Final_task.py
@@ -1,6 +1,13 @@
+import pandas as pd #добавлено
 import random
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
+# Создаем столбцы, которые заполняем 0 и 1 при выполнении условий
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot'] = '0'
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human'] = '0'
+print(data)
